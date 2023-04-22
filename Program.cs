47d1a6fc24b63d0d3470f9e33800eda1e30bbf39
jay75chauhan <chauhan75jayk@gmail.com>
2023-04-22 12:32:10 +0530

    add

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var configuration = builder.Configuration;
+
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # dotNet-crud-docker
+
+dotnet new webapi -n doNet-crud-docker
+
+dotnet add package Microsoft.EntityFrameworkCore.Design
+
+dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL
+
+dotnet run
